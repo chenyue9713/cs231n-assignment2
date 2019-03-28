Bug free for batch normalization, come back to complete rest of section after completed optim.py

cs231n/classifiers/fc_net.py
@@ -265,16 +265,16 @@ class FullyConnectedNet(object):
         net_x = X.copy()
         #net_x = np.reshape(net_x,[net_x.shape[0], -1])
         for i in range(self.num_layers -1):
-            fc_out, affine_caches['fc'+str(i+1)+'_cache']= affine_forward(net_x, self.params['W'+str(i+1)], self.params['b'+str(i+1)])
+            fc_out, affine_caches[str(i+1)]= affine_forward(net_x, self.params['W'+str(i+1)], self.params['b'+str(i+1)])
             
-            if self.normalization=='batchnorm':
-                bn_out, bn_caches['bn'+str(i+1)+'_cache'] = batchnorm_forward(fc_out, self.params['gamma'+str(i+1)], self.params['beta'+str(i+1)], self.bn_params[i])
-                relu_out, relu_caches['relu'+str(i+1)+'_cache']=relu_forward(bn_out)
+            if self.normalization =='batchnorm':
+                bn_out, bn_caches[str(i+1)] = batchnorm_forward(fc_out, self.params['gamma'+str(i+1)], self.params['beta'+str(i+1)], self.bn_params[i])
+                relu_out, relu_caches[str(i+1)]=relu_forward(bn_out)
             else:
-                relu_out, relu_caches['relu'+str(i+1)+'_cache']=relu_forward(fc_out)
+                relu_out, relu_caches[str(i+1)]=relu_forward(fc_out)
                 
             if self.use_dropout:
-                dp_out, dp_caches['dp'+str(i+1)+'_cache']= dropout_forward(relu_out,self.dropout_param)
+                dp_out, dp_caches[str(i+1)]= dropout_forward(relu_out,self.dropout_param)
                 net_x = dp_out.copy()
             else:
                 net_x = relu_out.copy()
@@ -315,20 +315,20 @@ class FullyConnectedNet(object):
         grads['b'+str(self.num_layers)] = db_last
         
         for i in range(self.num_layers-1,0,-1):
-            
+
             if self.use_dropout:
-                ddp = dropout_backward(dx_last,dp_caches['dp'+str(i)+'_cache'])
-                drelu = relu_backward(ddp, relu_caches['relu'+str(i)+'_cache'])
+                ddp = dropout_backward(dx_last,dp_caches[str(i)])
+                drelu = relu_backward(ddp, relu_caches[str(i)])
             else:
-                drelu = relu_backward(dx_last, relu_caches['relu'+str(i)+'_cache'])
+                drelu = relu_backward(dx_last, relu_caches[str(i)])
 
             if self.normalization=='batchnorm':
-                dbn, dgamma, dbeta = batchnorm_backward_alt(drelu, bn_caches['bn'+str(i)+'_cache'])
-                dx_last, dw_last, db_last = affine_backward(dbn, affine_caches['fc'+str(i)+'_cache'])
+                dbn, dgamma, dbeta = batchnorm_backward(drelu, bn_caches[str(i)])
+                dx_last, dw_last, db_last = affine_backward(dbn, affine_caches[str(i)])
                 grads['gamma'+str(i)] = dgamma
                 grads['beta'+str(i)] = dbeta
             else:
-                dx_last, dw_last, db_last = affine_backward(drelu, affine_caches['fc'+str(i)+'_cache'])
+                dx_last, dw_last, db_last = affine_backward(drelu, affine_caches[str(i)])
             
             grads['W'+str(i)] = dw_last + self.reg*self.params['W'+str(i)] 
             grads['b'+str(i)] = db_last

cs231n/layers.py
@@ -193,7 +193,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
                 'gamma': gamma,
                 'normalized_x': normalized_x,
                 'sqrtvar':np.sqrt(sample_var + eps),
-                'ivar':1./np.sqrt(sample_var + eps),
+                'ivar':1.0/np.sqrt(sample_var + eps),
                 'x_minus_sample_mean':(x - sample_mean),
                     }
         
@@ -211,7 +211,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # Store the result in the out variable.                               #
         ######################################################################
         #running_var = (N/(N-1))*running_var
-        out = (gamma/np.sqrt(running_var+eps)) * x + (beta - (gamma*running_mean)/np.sqrt(running_var+eps))
+        out = (gamma / (np.sqrt(running_var + eps)) * x) + (beta - (gamma*running_mean)/np.sqrt(running_var + eps))
         
         #######################################################################
         #                          END OF YOUR CODE                           #
@@ -262,7 +262,7 @@ def batchnorm_backward(dout, cache):
     dbeta = np.sum(dout, axis=0)
     
     dxhat = dout * gamma
-    dvar = np.sum(dxhat*x_minus_sample_mean*ivar*ivar**2*(-1./2.),axis=0)
+    dvar = np.sum(dxhat*x_minus_sample_mean*ivar*ivar**2*(-0.5),axis=0)
     dmean = np.sum(dxhat*(-ivar),axis=0) + (1./N)*dvar*np.sum(x_minus_sample_mean*(-2),axis=0)
     
     dx = dxhat * ivar + dvar * (2*x_minus_sample_mean/N) + dmean/N
@@ -309,7 +309,7 @@ def batchnorm_backward_alt(dout, cache):
     dbeta = np.sum(dout, axis=0)
     
      # Alternative faster formula way of calculating dx. ref: http://cthorey.github.io./backpropagation/
-    dx =(1 / N) * gamma * 1/sqrtvar * ((N * dout) - np.sum(dout, axis=0) - (x_minus_sample_mean) * np.square(ivar) * np.sum(dout * (x_minus_sample_mean), axis=0))
+    dx = (1 / N) * gamma * 1/sqrtvar * ((N * dout) - np.sum(dout, axis=0) - (x_minus_sample_mean) * np.square(ivar) * np.sum(dout * (x_minus_sample_mean), axis=0))
 
     ###########################################################################
     #                             END OF YOUR CODE                            #